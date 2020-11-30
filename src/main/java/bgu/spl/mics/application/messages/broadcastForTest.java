60e79e9842ec6672a614dd8883722950983b9d78
Dan <danlu@post.bgu.ac.il>
2020-11-30 18:59:57 +0200

    finished unit tests

---
+++ src/main/java/bgu/spl/mics/application/messages/broadcastForTest.java
@@ -0,0 +1,8 @@
+package bgu.spl.mics.application.messages;
+
+import bgu.spl.mics.Broadcast;
+import bgu.spl.mics.Event;
+
+public class broadcastForTest implements Broadcast {
+
+}
--- src/test/java/bgu/spl/mics/MessageBusImplTest.java
+++ src/test/java/bgu/spl/mics/MessageBusImplTest.java
@@ -1,12 +1,14 @@
 package bgu.spl.mics;
 
 import bgu.spl.mics.application.messages.AttackEvent;
+import bgu.spl.mics.application.messages.broadcastForTest;
 import bgu.spl.mics.application.services.C3POMicroservice;
+import bgu.spl.mics.application.services.R2D2Microservice;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class MessageBusImplTest {
 
@@ -32,24 +34,62 @@ class MessageBusImplTest {
 
     @Test
     void complete() {//
+        MicroService m = new C3POMicroservice();
+        mb.register(m);
+        mb.subscribeEvent(AttackEvent.class, m);
+        AttackEvent e = new AttackEvent();
+        Future<Boolean> f = mb.sendEvent(e);
+        mb.complete(e,true);
+        try {
+            assertTrue(f.get());
+        } catch (Exception any) {fail("");}
+        mb.register(m);//return mb to its initialized state
 
     }
 
     @Test
     void sendBroadcast() {//
+        Broadcast b = new broadcastForTest();
+        MicroService mc = new C3POMicroservice();
+        MicroService mr = new R2D2Microservice(41);
+
+        mb.register(mc);
+        mb.register(mr);
+        mb.subscribeBroadcast(b.getClass() ,mc);
+        mb.subscribeBroadcast(b.getClass() ,mr);
+        mb.sendBroadcast(b);
+        try {
+            assertEquals(b, mb.awaitMessage(mc));
+            assertEquals(b, mb.awaitMessage(mr));
+        } catch (Exception ex) {fail("broadcast wasn't received by all");}
+
+        mb.unregister(mc);
+        mb.unregister(mr); //return mb to its initialized state
     }
 
     @Test
-    void sendEvent() {//
-        MicroService m1 = new C3POMicroservice();
-        mb.register(m1);
-        Event<Integer> e = new AttackEvent();
-        mb.subscribeEvent(e ,m1);
+    void sendEvent() {
+        AttackEvent e = new AttackEvent();
+        MicroService m = new C3POMicroservice();
+        //test that null is returned for an even with no subscribers
+        assertNull(mb.sendEvent(e));
 
+
+        mb.register(m);
+        mb.subscribeEvent(e.getClass() ,m);
+        //test that Future returned is not null if there are subscribers
+        assertNotNull(mb.sendEvent(e));
+
+        try {//test that massage arrived to the subscriber
+            assertEquals(e, mb.awaitMessage(m));
+        } catch (Exception ex) {fail("event didn't arrive");}
+
+        mb.unregister(m); //return mb to its initialized state
     }
 
     @Test
     void register() {
+        //the register functionality is tested in other functions
     }
 
     @Test
@@ -59,5 +99,22 @@ class MessageBusImplTest {
 
     @Test
     void awaitMessage() {
+        MicroService m = new C3POMicroservice();
+        try {
+            mb.awaitMessage(m);
+            fail("exception was not thrown");
+        } catch (Exception e) {
+            assertTrue(e instanceof IllegalStateException);
+        }
+        mb.register(m);
+        mb.subscribeEvent(AttackEvent.class, m);
+        AttackEvent e = new AttackEvent();
+        mb.sendEvent(e);
+        try {
+            assertEquals(e, mb.awaitMessage(m));
+        } catch (Exception ex) {fail();}
+
+        mb.unregister(m); //return mb to its initialized state
     }
+
 }
