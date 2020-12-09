285966a546db7c19396742294b2dc189151d4171
Dan <danlu@post.bgu.ac.il>
2020-12-09 15:36:41 +0200

    msg bus progress

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,5 +1,9 @@
 package bgu.spl.mics;
 
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
@@ -8,6 +12,9 @@ package bgu.spl.mics;
 public class MessageBusImpl implements MessageBus {
 	
 	private static MessageBusImpl instance = null;
+	private Map<Event<?>, Future<?>> futureMap;
+	private Map<MicroService, LinkedBlockingQueue<Message>> microserviceQ;
+	private Map<Class<? extends Message>, LinkedBlockingQueue<MicroService>> msgQ;
 
 	private MessageBusImpl(){
 
@@ -15,32 +22,56 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		
+		subscribeMessage(type, m);
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		
+		subscribeMessage(type, m);
     }
+
+    private void subscribeMessage(Class<? extends Message> type, MicroService m) {
+		msgQ.putIfAbsent(type, new LinkedBlockingQueue<>());
+		msgQ.get(type).add(m);
+	}
 
 	@Override @SuppressWarnings("unchecked")
 	public <T> void complete(Event<T> e, T result) {
+		Future f = futureMap.get(e);
+		f.resolve(result);
+		//futureMap.get(e).resolve(result); TODO
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		
+		BlockingQueue<MicroService> q = msgQ.get(b.getClass());
+		if (q != null) {
+			for (MicroService m : q) {
+				microserviceQ.get(m).add(b);
+			}
+			//TODO notify all?
+		}
 	}
 
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		
+		Future<T> future = new Future<>();
+		BlockingQueue<MicroService> q = msgQ.get(e.getClass());
+		if (q != null) {
+			MicroService next = q.poll();
+			if (next != null) {
+				microserviceQ.get(next).add(e);
+				futureMap.put(e, future);
+				q.add(next); //return  to end of the queue
+				return future;
+			}
+		}
         return null;
 	}
 
 	@Override
 	public void register(MicroService m) {
-		
+		microserviceQ.put(m, new LinkedBlockingQueue<>());
 	}
 
 	@Override
